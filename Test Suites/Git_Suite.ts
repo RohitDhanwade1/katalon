f0a9c7f9dcba3a235c886ec3a9b8a7f3ad341b31
Rohit Dhanwade <Rohit Dhanwade@DS009.step.com>
2023-06-28 18:07:42 +0530

    First Commit

---
+++ Test Suites/Git_Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Git_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2654ffa3-2375-4396-bfb7-c4b707660d28</testSuiteGuid>
+   <testCaseLink>
+      <guid>b8afbb8b-0cd3-4cd6-8534-682dcb0a4e8c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/first_Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
